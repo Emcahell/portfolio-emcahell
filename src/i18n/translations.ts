483a1a8764c95eab7ea9daf81bbd9c42caa003b3
emcahell <emmanuelltowers@gmail.com>
2026-05-25 19:22:10 -0400

    update informations projects in translation

--- src/i18n/translations.ts
+++ src/i18n/translations.ts
@@ -9,46 +9,48 @@ export const translations: Translations = {
     downloadCV: "Download CV",
     greeting: "I'm a",
     projects: "My Projects",
-    aboutMe: "I'm a self-taught web developer with a passion for learning and creating. I enjoy working on projects that allow me to solve problems and create something useful for others.",
+    aboutMe:
+      "I'm a self-taught web developer with a passion for learning and creating. I enjoy working on projects that allow me to solve problems and create something useful for others.",
     projectStatistics:
       "An SPA to get the average and percentage of attendance of the monthly school enrollment.",
     projectStatesVzla:
-      "Know the states of Venezuela and their relevant data, search by state and save your favorites.",
+      "This is an website for finding information about Venezuela's current states and relevant data, you can search by state and save your favorites.",
     projectDistrictsPanem:
-      "Web site to know the 12 districts of Panem, their location, their industry and their victors.",
+      "Web site with information about the 12 districts of Panem and their location, industry, and winner.",
     projectTop:
       "Web site with information about Twenty One Pilots, their origin, albums (with their eras), lore and some interesting facts about their members.",
     projectKefren:
       "Portfolio for Kefren Art, a digital artist. It shows his work, his social networks and contact information.",
     projectOneStarlink:
-      "Landing page for One Starlink, it shows information about products and services they offer.",
+      "A landing page that has been created for One Starlink, provides information about products and services  offered.",
     projectChatIaEmma:
-      "Artificial intelligence chatbot with instructions to respond with Venezuelan dialect.",
+      "Chatbot with an artificial intelligence capable of responding in Venezuelan dialect.",
     projectOptimizeImage:
-      "Web to optimize images, compressing then and changing their format to WebP.",
+      "Page Web for optimizing images, compressing them, and changing their format to WebP.",
     skills: "My Skills",
   },
   es: {
     downloadCV: "Descargar CV",
     greeting: "Soy un",
     projects: "Mis Proyectos",
-    aboutMe: "Soy un desarrollador web autodidacta con pasión por aprender y crear. Disfruto trabajando en proyectos que me permiten crear algo útil para los demás.",
+    aboutMe:
+      "Soy un desarrollador web autodidacta con pasión por aprender y crear. Disfruto trabajando en proyectos que me permiten crear algo útil para los demás.",
     projectStatistics:
-      "Una SPA para obtener el promedio y porcentaje de asistencia de la matricula escolar mensual.",
+      "SPA que obtiene el promedio y porcentaje de asistencia de la matricula escolar mensual.",
     projectStatesVzla:
-      "Conoce los estados de Venezuela y sus datos relevantes, busca por estado y guarda tus favoritos.",
+      "Web para conocer los estados de Venezuela y sus datos relevantes, puedes buscar por estado y guardar a tus favoritos.",
     projectDistrictsPanem:
-      "Sitio Web para conocer a los 12 distritos de Panem, su ubicación, su industria y sus vencedores.",
+      "Sitio Web con información sobre los 12 distritos de Panem, su ubicación, su industria y sus vencedores.",
     projectTop:
       "Sitio Web con información sobre Twenty One Pilots, origen de la banda, albums (con sus eras), lore y algunas curiosidades sobre sus integrantes.",
     projectKefren:
       "Portafolio para Kefren Art, un artista digital. Muestra sus trabajos, sus redes sociales e información de contacto.",
     projectOneStarlink:
-      "Landing page para One Starlink, muestra información sobre productos y servicios que ofrecen.",
+      "Landing Page hecha para One Starlink, muestra información sobre productos y servicios que ofrecen.",
     projectChatIaEmma:
-      "Chatbot de inteligencia artificial con instrucciones para que responda con dialecto venezolano.",
+      "Chatbot con inteligencia artificial que responde con dialecto venezolano.",
     projectOptimizeImage:
-      "Web para optimizar imágenes, comprimiendolas y cambiando su formato a WebP.",
+      "Pagina Web para optimizar imágenes, comprimiendolas y cambiando su formato a WebP.",
     skills: "Mis Habilidades",
   },
 };
